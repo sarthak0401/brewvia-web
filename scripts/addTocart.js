d0808ff3fcd3a3f18c7b45ba5cb0b6a57ddec88c
sarthak0401 <sarthakbokade413@gmail.com>
2026-01-02 17:47:47 +0530

    Improved Cart UI

--- scripts/addTocart.js
+++ scripts/addTocart.js
@@ -5,12 +5,17 @@ const toast = document.getElementById("toast");
 
 let toastTimer;
 
+// const btnDivContainer = document.getElementsByClassName("addBtn-div-container");
+
 function saveCart() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 addToCartButtons.forEach((button) =>
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
+    console.log("target:", e.target);
+    console.log("currentTarget:", e.currentTarget);
+
     const productName = button.getAttribute("data-name").trim();
     const price = Number(button.dataset.price);
 
@@ -18,7 +23,7 @@ addToCartButtons.forEach((button) =>
     clearTimeout(toastTimer);
 
     toast.innerHTML = `<span class="toast-highlight"> ${productName}</span>added to cart! `;
-    toast.className = "show";
+    toast.classList.add("show");
 
     const existingItem = cart.find((item) => item.productName === productName);
     if (existingItem) {
@@ -28,17 +33,43 @@ addToCartButtons.forEach((button) =>
       cart.push({ productName, price, quantity: 1 });
       saveCart();
     }
+
+    btnChange(existingItem, e.target);
+
     updateCartUI();
     console.log(cart);
 
-    toastTimer = setTimeout(function () {
-      toast.className = toast.className.replace("show", "");
+    toastTimer = setTimeout(() => {
+      console.log("Hiding toast");
+      toast.classList.remove("show");
     }, 3000);
   })
 );
 
-const cartCountElements = document.querySelectorAll(".cart-count");
-const cartPrice = document.getElementById("cart-price");
+function btnChange(existingItem, clickedButton) {
+  const container = clickedButton.closest(".addBtn-div-container");
+
+  if (!container) return;
+
+  const qty = existingItem ? existingItem.quantity : 1;
+
+  container.innerHTML = `<div class="qty-control">
+ <button class="qty-btn decrement">-</button>
+ <span class="qty-value">${qty}</span>
+ <button class="qty-btn increment">+</button>
+</div>`;
+}
+
+let cartPrice = null;
+let cartCountElements = [];
+
+// We needed this function, because without this we were accessing this cart-price, cart-count elements without them being injected by header.html via header.js so it was assigning null to these variables.
+function initHeaderElements() {
+  cartPrice = document.getElementById("cart-price");
+  cartCountElements = document.querySelectorAll(".cart-count");
+
+  updateCartUI();
+}
 
 function updateCartUI() {
   let sum = 0;
